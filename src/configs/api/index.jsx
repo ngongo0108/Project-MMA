88bb7e2ca874ae0b07f138f1419d1158c7d4d29f
Ngơ Ngơ <thuhuong562002@gmail.com>
2024-04-23 14:03:39 +0700

    v1

--- src/configs/api/index.jsx
+++ src/configs/api/index.jsx
@@ -1,4 +1,3 @@
-
-const API_URL_ENV = "https://5de1-2402-800-6374-7a22-c59c-6f15-8fd4-5a7c.ngrok-free.app/api";
+API_URL_ENV = "https://2afd-42-119-149-124.ngrok-free.app/api";
 
 export default API_URL_ENV;
